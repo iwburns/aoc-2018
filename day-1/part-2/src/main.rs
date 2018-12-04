fb3562a25a3196f3197b765ac5533fbd83fe08e0
Ian <iwburns8@gmail.com>
2018-12-03 23:40:17 -0600

    add solution to day-1 part-2

---
+++ day-1/part-2/src/main.rs
@@ -0,0 +1,65 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input = include_str!("./input.txt");
+    let total = get_total(input);
+    println!("{}", total);
+}
+
+fn get_total(input: &str) -> i32 {
+    let mut frequencies = HashSet::new();
+
+    let deltas = input
+        .lines()
+        .map(into_number)
+        .cycle();
+
+    let mut total = 0;
+
+    for delta in deltas {
+        frequencies.insert(total);
+
+        total += delta;
+
+        if frequencies.contains(&total) {
+            break;
+        }
+    }
+
+    total
+}
+
+fn into_number(line: &str) -> i32 {
+    let (sign, num) = line.split_at(1);
+
+    let amount: i32 = num.parse().expect("couldn't parse number from line");
+
+    match sign {
+        "+" => amount,
+        "-" => -amount,
+        _ => unreachable!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = "+1\n-1";
+        assert_eq!(get_total(input), 0);
+    }
+
+    #[test]
+    fn test_2() {
+        let input = "+3\n+3\n+4\n-2\n-4";
+        assert_eq!(get_total(input), 10);
+    }
+
+    #[test]
+    fn test_3() {
+        let input = "+7\n+7\n-2\n-7\n-4";
+        assert_eq!(get_total(input), 14);
+    }
+}
