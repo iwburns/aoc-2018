f078e93297bea9dbf87c1ff9f9e7f539961eb0d1
Ian <iwburns8@gmail.com>
2018-12-04 00:14:19 -0600

    add solution to day-2 part-1

---
+++ day-2/part-1/src/main.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = include_str!("./input.txt");
+    let total = get_checksum(input);
+    println!("{}", total);
+}
+
+fn get_checksum(input: &str) -> u32 {
+    let (twos, threes) = input
+        .lines()
+        .map(into_counts)
+        .fold((0, 0), |acc, curr| (acc.0 + curr.0, acc.1 + curr.1));
+    twos * threes
+}
+
+fn into_counts(line: &str) -> (u32, u32) {
+    let char_counts = line.chars()
+        .fold(HashMap::new(), move |mut acc, curr| {
+            *acc.entry(curr).or_insert(0) += 1;
+            acc
+        });
+
+    let (twos, threes) = char_counts.iter()
+        .fold((0, 0), |(twos, threes), (_char, count)| {
+            match count {
+                2 => ((twos + 1).min(1), threes),
+                3 => (twos, (threes + 1).min(1)),
+                _ => (twos, threes),
+            }
+        });
+
+    (twos, threes)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab";
+        assert_eq!(get_checksum(input), 12);
+    }
+}
