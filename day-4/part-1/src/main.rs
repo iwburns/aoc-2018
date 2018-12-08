fcad0dbb1fc2fd76610dcf7ae59e7dfb16a147a1
Ian <iwburns8@gmail.com>
2018-12-08 14:49:08 -0600

    re-factor d4/p1 to use iterators a bit better than before

--- day-4/part-1/src/main.rs
+++ day-4/part-1/src/main.rs
@@ -52,20 +52,39 @@ fn main() {
 }
 
 fn get_solution(input: &str) -> u32 {
-    let mut records = input.lines().map(Record::from).collect::<Vec<Record>>();
+    let records = get_records(input);
 
-    records.sort_by_key(|r| r.timestamp);
+    let minutes_asleep = get_each_minute_spent_asleep_per_guard(&records);
 
-    let minutes_spent_asleep = get_each_minute_spent_asleep_per_guard(&records);
+    let (most_sleepy_guard, _total_slept) = minutes_asleep
+        .iter()
+        .max_by_key(|(_guard_id, minute_sleep_amounts)| {
+            minute_sleep_amounts
+                .iter()
+                .map(|(_minute, &times_slept)| times_slept)
+                .sum::<u32>()
+        })
+        .expect("somehow no guards have slept");
 
-    let guard_id = find_guard_who_has_slept_the_most(&minutes_spent_asleep);
+    let (most_slept_minute, _times_slept) = minutes_asleep
+        .get(&most_sleepy_guard)
+        .expect("somehow our sleepy guard isn't in the list of sleeping guards")
+        .iter()
+        .max_by_key(|(_minute, &times_slept)| times_slept)
+        .expect("somehow our sleepy guard hasn't slept");
 
-    let minute = get_minute_guard_was_asleep_most_often(&minutes_spent_asleep, guard_id);
-
-    minute * guard_id
+    most_sleepy_guard * most_slept_minute
 }
 
-fn get_each_minute_spent_asleep_per_guard(records: &[Record]) -> HashMap<GuardId, Vec<Minute>> {
+fn get_records(input: &str) -> Vec<Record> {
+    let mut records: Vec<Record> = input.lines().map(Record::from).collect();
+    records.sort_by_key(|r| r.timestamp);
+    records
+}
+
+fn get_each_minute_spent_asleep_per_guard(
+    records: &[Record],
+) -> HashMap<GuardId, HashMap<Minute, u32>> {
     let mut minutes_spent_asleep = HashMap::new();
 
     let mut current_guard = None;
@@ -86,73 +105,17 @@ fn get_each_minute_spent_asleep_per_guard(records: &[Record]) -> HashMap<GuardId
                 let minute_started = started_sleeping.time().minute();
                 let num_minutes_slept = record.timestamp.sub(started_sleeping).num_minutes();
 
-                let m = minutes_spent_asleep
+                let times_minute_slept: &mut HashMap<Minute, u32> = minutes_spent_asleep
                     .entry(guard_id)
-                    .or_insert_with(Vec::new);
+                    .or_insert_with(HashMap::new);
 
                 for i in 0..num_minutes_slept {
-                    m.push(minute_started + (i as u32));
+                    let curr_minute = minute_started + (i as u32);
+                    *times_minute_slept.entry(curr_minute).or_insert(0) += 1;
                 }
             }
         }
     }
 
     minutes_spent_asleep
-}
-
-fn find_guard_who_has_slept_the_most(
-    minutes_spent_asleep_per_guard: &HashMap<GuardId, Vec<Minute>>,
-) -> GuardId {
-    let (guard_id, _) =
-        minutes_spent_asleep_per_guard
-            .keys()
-            .fold((0, 0), |(guard, minutes), curr_guard| {
-                let minutes_spent_asleep = minutes_spent_asleep_per_guard
-                    .get(curr_guard)
-                    .expect("key must be present")
-                    .len();
-
-                if minutes_spent_asleep > minutes {
-                    return (*curr_guard, minutes_spent_asleep);
-                }
-
-                (guard, minutes)
-            });
-
-    guard_id
-}
-
-fn get_minute_guard_was_asleep_most_often(
-    minutes_spent_asleep_per_guard: &HashMap<GuardId, Vec<Minute>>,
-    guard_id: GuardId,
-) -> Minute {
-    let mut num_times_each_minute_was_spent_asleep: HashMap<Minute, u32> = HashMap::new();
-
-    let all_minutes_guard_spent_asleep: &Vec<Minute> = minutes_spent_asleep_per_guard
-        .get(&guard_id)
-        .expect("key must be present");
-    for minute in all_minutes_guard_spent_asleep {
-        *num_times_each_minute_was_spent_asleep
-            .entry(*minute)
-            .or_insert(0) += 1;
-    }
-
-    get_most_slept_minute(&num_times_each_minute_was_spent_asleep)
-}
-
-fn get_most_slept_minute(num_times_each_minute_was_spent_asleep: &HashMap<Minute, u32>) -> Minute {
-    let (minute, _) = num_times_each_minute_was_spent_asleep.keys().fold(
-        (0, 0),
-        |(min, amt_slept), curr_minute| {
-            let times_asleep = num_times_each_minute_was_spent_asleep
-                .get(curr_minute)
-                .expect("key must be present");
-            if *times_asleep > amt_slept {
-                return (*curr_minute, *times_asleep);
-            }
-            (min, amt_slept)
-        },
-    );
-
-    minute
 }
