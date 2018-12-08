bd3e4aa3702eeeee0f136fcc484bbf0fa3b30d6c
Ian <iwburns8@gmail.com>
2018-12-08 14:00:08 -0600

    switch to Rust 2018 and run cargo fix, cargo fmt, and clippy

--- day-4/part-2/src/main.rs
+++ day-4/part-2/src/main.rs
@@ -1,11 +1,9 @@
-extern crate chrono;
-
-use chrono::DateTime;
-use chrono::Utc;
 use chrono::offset::TimeZone;
+use chrono::DateTime;
+use chrono::Timelike;
+use chrono::Utc;
 use std::collections::HashMap;
 use std::ops::Sub;
-use chrono::Timelike;
 
 type Minute = u32;
 type GuardId = u32;
@@ -23,28 +21,27 @@ struct Record {
     event: GuardEvent,
 }
 
-impl<'a> From<&'a str> for Record {
-    fn from(string: &'a str) -> Self {
+impl From<&str> for Record {
+    fn from(string: &str) -> Self {
         let (time_str, event_str) = string.split_at(18);
 
-        let timestamp = Utc.datetime_from_str(time_str, "[%Y-%m-%d %H:%M]")
+        let timestamp = Utc
+            .datetime_from_str(time_str, "[%Y-%m-%d %H:%M]")
             .expect("couldn't parse date");
 
         let event = match event_str.trim() {
             "wakes up" => GuardEvent::WakeUp,
             "falls asleep" => GuardEvent::FallAsleep,
             s => {
-                let guard_num = s.trim_matches(|c: char| !c.is_numeric())
+                let guard_num = s
+                    .trim_matches(|c: char| !c.is_numeric())
                     .parse()
                     .expect("couldn't parse guard number");
                 GuardEvent::BeginShift(guard_num)
-            },
+            }
         };
 
-        Record {
-            timestamp,
-            event,
-        }
+        Record { timestamp, event }
     }
 }
 
@@ -55,39 +52,42 @@ fn main() {
 }
 
 fn get_solution(input: &str) -> u32 {
-    let mut records = input.lines()
-        .map(Record::from)
-        .collect::<Vec<Record>>();
+    let mut records = input.lines().map(Record::from).collect::<Vec<Record>>();
 
     records.sort_by_key(|r| r.timestamp);
 
     let minutes_spent_asleep = get_each_minute_spent_asleep_per_guard(&records);
 
-    let (guard_id, most_slept_minute, _times_slept) = minutes_spent_asleep.keys()
-        .fold((0, 0, 0), |(g_acc, m_acc, t_acc), guard_id| {
+    let (guard_id, most_slept_minute, _times_slept) =
+        minutes_spent_asleep
+            .keys()
+            .fold((0, 0, 0), |(g_acc, m_acc, t_acc), guard_id| {
+                let minutes_slept_by_guard = minutes_spent_asleep.get(guard_id).expect("it exists");
 
-            let minutes_slept_by_guard = minutes_spent_asleep.get(guard_id).expect("it exists");
+                let (minute, count) =
+                    minutes_slept_by_guard
+                        .keys()
+                        .fold((0, 0), |(min_acc, c_acc), minute| {
+                            let num = minutes_slept_by_guard.get(minute).expect("it exists");
+                            if *num > c_acc {
+                                return (*minute, *num);
+                            }
+                            (min_acc, c_acc)
+                        });
 
-            let (minute, count) = minutes_slept_by_guard.keys()
-                .fold((0, 0), |(min_acc, c_acc), minute| {
-                    let num = minutes_slept_by_guard.get(minute).expect("it exists");
-                    if *num > c_acc {
-                        return (*minute, *num);
-                    }
-                    (min_acc, c_acc)
-                });
+                if count > t_acc {
+                    return (*guard_id, minute, count);
+                }
 
-            if count > t_acc {
-                return (*guard_id, minute, count);
-            }
-
-            (g_acc, m_acc, t_acc)
-        });
+                (g_acc, m_acc, t_acc)
+            });
 
     guard_id * most_slept_minute
 }
 
-fn get_each_minute_spent_asleep_per_guard(records: &[Record]) -> HashMap<GuardId, HashMap<Minute, u32>> {
+fn get_each_minute_spent_asleep_per_guard(
+    records: &[Record],
+) -> HashMap<GuardId, HashMap<Minute, u32>> {
     let mut minutes_spent_asleep = HashMap::new();
 
     let mut current_guard = None;
@@ -97,10 +97,10 @@ fn get_each_minute_spent_asleep_per_guard(records: &[Record]) -> HashMap<GuardId
         match record.event {
             GuardEvent::BeginShift(guard_id) => {
                 current_guard = Some(guard_id);
-            },
+            }
             GuardEvent::FallAsleep => {
                 time_fell_asleep = Some(record.timestamp);
-            },
+            }
             GuardEvent::WakeUp => {
                 let guard_id = current_guard.expect("a guard hasn't started their shift yet");
                 let started_sleeping = time_fell_asleep.expect("this guard isn't asleep");
@@ -108,13 +108,15 @@ fn get_each_minute_spent_asleep_per_guard(records: &[Record]) -> HashMap<GuardId
                 let minute_started = started_sleeping.time().minute();
                 let num_minutes_slept = record.timestamp.sub(started_sleeping).num_minutes();
 
-                let mut m = minutes_spent_asleep.entry(guard_id).or_insert(HashMap::new());
+                let m = minutes_spent_asleep
+                    .entry(guard_id)
+                    .or_insert_with(HashMap::new);
 
                 for i in 0..num_minutes_slept {
                     let curr_minute = minute_started + (i as u32);
                     *m.entry(curr_minute).or_insert(0) += 1;
                 }
-            },
+            }
         }
     }
 
